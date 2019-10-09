b279e66b15139335d8b14e6f0f9b9c196e8ae0c7
Dalton <dalton84@gmail.com>
2019-10-08 21:15:20 -0300

    meu primeiro commit

---
+++ src/main/java/br/downtown/BlogRespository.java
@@ -0,0 +1,15 @@
+package br.downtown;
+
+import br.downtown.domain.Blog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BlogRespository extends JpaRepository<Blog, Integer> {
+
+    // custom query to search to blog post by title or content
+    List<Blog> findByTitleContainingOrContentContaining(String text, String textAgain);
+
+}
